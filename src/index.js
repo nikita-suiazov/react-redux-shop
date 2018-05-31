fb3c2d0087d9adf95f2eb9015e056502563969d2
Nikita <jumper211212@gmail.com>
2018-05-31 07:57:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './main.css'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import { syncHistoryWithStore } from 'react-router-redux'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import { Provider } from 'react-redux'
 
 import reducers from 'reducers'
@@ -15,7 +14,6 @@ import Phones from 'containers/Phones'
 import Phone from 'containers/Phone'
 import Basket from 'containers/Basket'
 
-
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
 const history = syncHistoryWithStore(browserHistory, store)
@@ -23,9 +21,9 @@ const history = syncHistoryWithStore(browserHistory, store)
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <Route component={Layout}>
-        <Route path='/' component={Phones} />
-        <Route path='/categories/:id' component={Phones} />
+      <Route path='/' component={Layout}>
+        <IndexRoute component={Phones} />
+        <Route path='categories/:id' component={Phones} />        
       </Route>
       <Route path='/phones/:id' component={Phone} />
       <Route path='/basket' component={Basket} />
